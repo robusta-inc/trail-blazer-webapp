aebafce4e937ccdda9a6b863ab418974c9872042
Sudhir Ravindramohan <sudhir.ravindramohan@gmail.com>
2013-05-07 03:52:53 +0530

    Serializing session attr

--- src/main/java/trail.blazer.app/TrailBlazer.java
+++ src/main/java/trail.blazer.app/TrailBlazer.java
@@ -1,10 +1,11 @@
 package trail.blazer.app;
 
+import java.io.Serializable;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
 
-public class TrailBlazer implements Iterable<String>{
+public class TrailBlazer implements Iterable<String>, Serializable {
     List<String> theTrail = new ArrayList<String>();
 
     public TrailBlazer addThis(String toAddTo) {
